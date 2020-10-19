8bab91be3a7a87741c5ee7bf292a6d3680e6f8e7
Alexander Usolcev <alexander.usolcev@gmail.com>
2020-10-19 22:31:09 +0200

    feat (copyright): update copyright
    Updated copyright.

--- Modules/Party.lua
+++ Modules/Party.lua
@@ -1,7 +1,7 @@
 --[[
     Appreciate what others people do. (c) Usoltsev
 
-    Copyright (c) <2016-2019>, Usoltsev <alexander.usolcev@gmail.com> All rights reserved.
+    Copyright (c) <2016-2020>, Usoltsev.
 
     Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
     Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
